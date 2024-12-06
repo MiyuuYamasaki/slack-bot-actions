93d57a318097b82b22b56b961cc67715081781e3
MiyuuYamasaki <y-miyu-2429@occ.co.jp>
2024-12-06 10:58:39 +0900

    edit CHANNEL_ID

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { WebClient } = require('@slack/web-api');
 
 // Slackのトークンを環境変数から取得
 const SLACK_TOKEN = process.env.SLACK_TOKEN;
-const CHANNEL_ID = 'C07HCPM5HD5'; // 送信先のチャンネルID
+const CHANNEL_ID = 'C07MXF5NHUG'; // 送信先のチャンネルID
 
 // Slack Web APIクライアントを初期化
 const client = new WebClient(SLACK_TOKEN);
@@ -12,7 +12,7 @@ const client = new WebClient(SLACK_TOKEN);
     // メッセージを送信
     const result = await client.chat.postMessage({
       channel: CHANNEL_ID,
-      text: 'おはようございます！', // 送信するメッセージ
+      text: 'test message', // 送信するメッセージ
     });
     console.log('Message sent: ', result.ts);
   } catch (error) {
